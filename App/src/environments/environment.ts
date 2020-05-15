9d754a73b1f8180008535a191ceeea71b55dbf66
@MizaelGalvez <mizaelgalvez@gmail.com>
2020-05-15 13:04:58 -0700

    Update environment.ts

--- App/src/environments/environment.ts
+++ App/src/environments/environment.ts
@@ -8,13 +8,6 @@ export const environment = {
 
 
 export const firebaseConfig = {
-  apiKey: 'AIzaSyDsymr6GkvB8t_M4fCDGL2bizcD0IRx2tU',
-  authDomain: 'dbs-de-proyectos.firebaseapp.com',
-  databaseURL: 'https://dbs-de-proyectos.firebaseio.com',
-  projectId: 'dbs-de-proyectos',
-  storageBucket: 'dbs-de-proyectos.appspot.com',
-  messagingSenderId: '471214404221',
-  appId: '1:471214404221:web:Me66422abdadc05c94f3714G'
 };
 
 
